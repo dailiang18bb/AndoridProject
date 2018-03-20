f49831efb4468c82a5709c4f57ad5eb462f60147
Charles <dailiang18bb@gmail.com>
2018-03-19 22:41:51 -0400

    HW4

--- CS639SpringHW4_LiangDai/app/src/main/java/com/example/android/cs639springhw4_liangdai/CircleAnimatoinView.java
+++ CS639SpringHW4_LiangDai/app/src/main/java/com/example/android/cs639springhw4_liangdai/CircleAnimatoinView.java
@@ -11,7 +11,6 @@ import android.util.TypedValue;
 import android.view.View;
 
 
-
 /**
  * Created by charles on 3/19/18.
  */
@@ -19,70 +18,61 @@ import android.view.View;
 public class CircleAnimatoinView extends View {
 
 
-
     private Paint mPaintAxis = new Paint();
+    int mCircleX = dpToPx(50);
+    int mCircleY = ;
+    int speed = 10;
+    int radius = dpToPx(20);
+    int flag = 0;
 
-
-
+    /**
+     * initial the paintbrush
+     */
     private void init() {
-
-
         mPaintAxis.setStrokeWidth(5);
         mPaintAxis.setAntiAlias(false);                       //设置画笔为无锯齿
         mPaintAxis.setColor(Color.BLACK);                    //设置画笔颜色
-        mPaintAxis.drawColor(Color.WHITE);                  //白色背景
-        mPaintAxis.setStrokeWidth((float) 3.0);              //线宽
-        mPaintAxis.setStyle(Paint.Style.STROKE);                   //空心效果
+        //mPaintAxis.setStyle(Paint.Style.STROKE);                   //空心效果
 
 
     }
 
 
-
-    //override the onDraw method
     @Override
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        // draw initial axis
-
-        canvas.drawCircle(dpToPx(50),dpToPx(50),dpToPx(10),mPaintAxis);
-
-
-
+        canvas.drawCircle(mCircleX, mCircleY, radius, mPaintAxis);
+        moveCircle(speed);
+        invalidate();
     }
-
-
-
 
 
     public CircleAnimatoinView(Context context) {
         super(context);
         init();
-
-
     }
-
-
 
 
     public CircleAnimatoinView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init();
-
-
-
     }
-
-
-
-
 
 
     private static int dpToPx(int dpValue) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpValue, Resources.getSystem().getDisplayMetrics());
     }
 
-
-
+    /**
+     * move the circle by the speed
+     */
+    private void moveCircle(int speed) {
+        if (flag == 0) {
+            mCircleX = mCircleX + speed;
+            if (mCircleX == getWidth() - radius) flag = 1;
+        } else if (flag == 1) {
+            mCircleX = mCircleX - speed;
+            if (mCircleX == radius) flag = 0;
+        }
+    }
 }
