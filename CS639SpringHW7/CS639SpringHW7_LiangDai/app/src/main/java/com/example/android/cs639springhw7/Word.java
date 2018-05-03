6bad65aa964fee758c276d74bcf9bdfbea8086b3
Charles <dailiang18bb@gmail.com>
2018-05-03 13:08:43 -0400

    Word class update

--- CS639SpringHW7/CS639SpringHW7_LiangDai/app/src/main/java/com/example/android/cs639springhw7/Word.java
+++ CS639SpringHW7/CS639SpringHW7_LiangDai/app/src/main/java/com/example/android/cs639springhw7/Word.java
@@ -9,10 +9,20 @@ import android.support.annotation.NonNull;
 @Entity(tableName = "word_table")
 public class Word {
 
-    @PrimaryKey
-    @NonNull
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+
     @ColumnInfo(name = "word")
     private String mWord;
+
+    @ColumnInfo(name = "word_prop")
+    private String mWordProp;
+
+    @ColumnInfo(name = "word_exp")
+    private String mWordExp;
+
 
     public Word(@NonNull String word) {
         this.mWord = word;
@@ -20,5 +30,21 @@ public class Word {
 
     public String getWord() {
         return this.mWord;
+    }
+
+    public void setmWordExp(@NonNull String mWordExp) {
+        this.mWordExp = mWordExp;
+    }
+
+    public String getmWordExp() {
+        return this.mWordExp;
+    }
+
+    public void setmWordProp(@NonNull String mWordProp) {
+        this.mWordProp = mWordProp;
+    }
+
+    public String getmWordProp() {
+        return this.mWordProp;
     }
 }
